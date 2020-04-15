4bed1575ecf54e5f773a92bef09e71ea3b428b4e
wanglifa <wanglifa1995@gmail.com>
2020-04-15 17:52:40 +0800

    处理body请求类型

--- src/helpers/until.ts
+++ src/helpers/until.ts
@@ -2,6 +2,9 @@ const toString = Object.prototype.toString
 export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
+export function isPlainObject (val: any): val is Object {
+  return toString.call(val) === '[object Object]'
+}
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
 }
