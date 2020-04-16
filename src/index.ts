f187d416b5e22aa6b9966d72f2ed29daeede74d4
wanglifa <wanglifa1995@gmail.com>
2020-04-16 15:26:35 +0800

    实现promise

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
 import { processHeaders } from './helpers/header'
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
