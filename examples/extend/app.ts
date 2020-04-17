d4efeb579a7e68e105953e0d73abbe1777d5d539
wanglifa <wanglifa1995@gmail.com>
2020-04-17 16:49:06 +0800

    返回数据支持泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,5 +1,30 @@
 import axios from '../../src/index'
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
 
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
 axios({
   url: '/extend/post',
   method: 'post',
@@ -8,14 +33,12 @@ axios({
   }
 })
 
-axios.request({
-  url: '/extend/post',
+axios('/extend/post', {
   method: 'post',
   data: {
     msg: 'hello'
   }
 })
-
 axios.get('/extend/get')
 
 axios.options('/extend/options')
--- src/axios.ts
+++ src/axios.ts
@@ -2,17 +2,12 @@ import { AxiosInstance } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/until'
 
-function createInstance(): any {
+function createInstance(): AxiosInstance {
   const context = new Axios()
-  for (const key in context) {
-    console.log(key, 'kkk')
-  }
   const instance = Axios.prototype.request.bind(context)
   //
   extend(instance, context)
-  console.log(instance, 'hhh')
-  console.log(context, 'con')
-  return instance
+  return instance as AxiosInstance
 }
 
 const axios = createInstance()
